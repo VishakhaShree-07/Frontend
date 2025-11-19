fa1e5a7c92358433392afaf0ee837fdb53b8f976
Vishakha Shree <vishakhashree4@gmail.com>
2025-11-19 21:38:30 +0530

    Change to querySelectorAll for selecting list items
    Updated the way list items are selected by using querySelectorAll instead of querySelector.

--- JavaScript/DOM/script.js
+++ JavaScript/DOM/script.js
@@ -1,10 +1,2 @@
-// const mainHeading = document.getElementById("title");
-// mainHeading.textContent = "Hello from JavaScript!";
-
-// const items = document.getElementsByClassName("list-item");
-// console.log(items);
-// items[0].textContent = "Hello World!";
-
-// const items = document.querySelector("li");
 const items = document.querySelectorAll("li");
 console.log(items); 
